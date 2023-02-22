c4510b5d871efa339f4ea32227da9233fd39b297
smylismart10 <nnajichineze93@gmail.com>
2023-02-21 20:35:33 -0800

    Nested Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,46 +1,25 @@
 #include "main.h"
-/**
- * times_table - print the 9 times table
- * Return: 0 (Always Success)
- */
-void times_table(void)
-{
-	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
 	{
-		for (b = 0; b <= 9; b++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			c = a * b;
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				if (b != 0)
-				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			_putchar(ch);
+			ch++;
 		}
-		
 		_putchar('\n');
+		i++;
 	}
 }
